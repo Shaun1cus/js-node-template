2f64730eb590ea15124528ecf310a03fd8c22493
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-02-26 19:14:03 -0600

    Add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,12 @@
+const fullName = 'Shaun Putnam';
+
+const courseName = 'Web Development I';
+
+const expectedGrade = 100;
+
+const message = fullName + ' is taking ' + courseName + ' and expects to earn ' + expectedGrade + '%.';
+
+const betterMessage = `${fullName} is taking ${courseName} and expects to earn ${expectedGrade}%.`;
+
+console.log(message);
+console.log(betterMessage);
