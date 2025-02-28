9e5110a7fab49742e9bb265d82381d2d0b2d54d1
Shaun Putnam <shaun.putnam@student.swic.edu>
2025-02-27 18:30:42 -0600

    Add function practice examples

--- src/index.js
+++ src/index.js
@@ -34,3 +34,44 @@ const battleRifle = {
 
 console.log(battleRifle.maxAmmo);
 console.log(battleRifle.range);
+
+const student = {
+	fullName: "Shaun Putnam",
+	course: "Web Development I",
+	grade: 100,
+	displayInfo() {
+		return `${fullName} is taking ${course} and expects to earn ${grade}%.`;
+	},
+};
+
+const student1 = {
+	name: "Sarah",
+	course: "JavaScript",
+	grade: 95,
+};
+
+const student2 = {
+	name: "John",
+	course: "Python",
+	grade: 88,
+};
+
+const student3 = {
+	name: "Alice",
+	course: "C++",
+	grade: 92,
+};
+
+function displayStudentInfo(student) {
+	return `${student.name} is taking ${student.course} and expects to earn ${student.grade}%.`;
+}
+
+console.log(displayStudentInfo(student1));
+console.log(displayStudentInfo(student2));
+console.log(displayStudentInfo(student3));
+
+function add2Nums(x, y) {
+	return x + y;
+}
+
+const sum = add2Nums(200, 300);
